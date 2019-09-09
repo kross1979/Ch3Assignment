efc44d7d0b6a9288f5a33b4ebae36008a0f14be3
kimbe <kimbe@KimberlyLaptop.attlocal.net>
2019-09-09 00:00:17 -0500

    Sales Tax

--- src/main/java/SalesTax.java
+++ src/main/java/SalesTax.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,7 +13,11 @@
  */
 public class SalesTax {
     public static void main(String[] args) {
-        
+        String purchasePrice = JOptionPane.showInputDialog("Please enter your purchase price:");
+        JOptionPane.showMessageDialog(null, purchasePrice\n + "Your state sales tax is " + purchasePrice*stateTax\n +
+                "Your city tax is " + purchasePrice*cityTax\n);
+        float stateTax = 0.04F;
+        float cityTax = 0.02F;
     }
    
 }
